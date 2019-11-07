4b73f860df70375d985c2a76f9fff97c7e6c8a3d
alajl <alajl@126.com>
2019-11-07 10:54:49 +0800

    add author

--- src/ThirdCodes/bmpWriter.java
+++ src/ThirdCodes/bmpWriter.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ThirdCodes;
 
 import java.awt.image.BufferedImage;
@@ -14,8 +9,9 @@ import java.io.IOException;
 
 /**
  *
- * @author l67liu
+ * @author liuli_user@126.com
  */
+
 public class bmpWriter {
 
     public static void write(BufferedImage img, File out) {
